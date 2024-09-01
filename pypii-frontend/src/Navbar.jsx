c0f37f436ae6c939a06125d363bf23c0c4161323
neelngawande <neelngawande@gmail.com>
2024-09-01 15:12:24 +0530

    commit 2

---
+++ pypii-frontend/src/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid d-flex justify-content-center">
+          <a className="navbar-brand text-success"><h2>PyPiiChecker</h2></a>
+        </div>
+      </nav>
+    </>
+  );
+}
